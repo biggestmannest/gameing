bdf5d97bfa1a8a1bee7b1672da997766e481d877
Big Man <54690435+biggestmannest@users.noreply.github.com>
2021-08-01 13:41:59 +0000

    game

---
+++ Gameing.plugin.js
@@ -0,0 +1,130 @@
+/**
+ * @name Gameing
+ * @website https://www.youtube.com/watch?v=AK0KXhWlDqM
+ * @source https://raw.githubusercontent.com/biggestmannest/gameing/main/Gameing.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/biggestmannest/gameing/main/Gameing.plugin.js
+ */
+
+module.exports = (() => {
+    const config = {
+        "info": {
+            "name": "Gameing",
+            "authors": [ {
+                "name": "me, the big man",
+                "discord_id": "459815318932553730",
+                "github_username": "biggestmannest"
+            } ],
+            "version": "1.0.0",
+            "description": "i game(ing)",
+            "github": "https://github.com/biggestmannest/gameing",
+            "github_raw": "https://raw.githubusercontent.com/biggestmannest/gameing/main/Gameing.plugin.js"
+        },
+        "main": "Gameing.plugin.js"
+    };
+
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {
+            this._config = config;
+        }
+        getName() {
+            return config.info.name;
+        }
+        getAuthor() {
+            return config.info.authors.map(a => a.name).join(", ");
+        }
+        getDescription() {
+            return config.info.description;
+        }
+        getVersion() {
+            return config.info.version;
+        }
+        load() {
+            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                confirmText: "Download Now",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                    });
+                }
+            });
+        }
+        start() { }
+        stop() { }
+    } : (([ Plugin, Api ]) => {
+        const plugin = (Plugin, Api) => {
+            const {
+                Patcher,
+                DiscordAPI,
+                Settings,
+                PluginUtilities
+            } = Api;
+
+            return class NitroPerks extends Plugin {
+                defaultSettings = {
+                    "screenSharing": true,
+                };
+                settings = PluginUtilities.loadSettings(this.getName(), this.defaultSettings);
+                originalNitroStatus = 0;
+                screenShareFix;
+
+                getSettingsPanel() {
+                    return Settings.SettingPanel.build(_ => this.saveAndUpdate(), ...[
+                        new Settings.SettingGroup("Features").append(...[
+                            new Settings.Switch("High Quality Screensharing", "you can game.", this.settings.screenSharing, value => this.settings.screenSharing = value)
+                        ]),
+                    ]);
+                }
+
+                saveAndUpdate() {
+                    PluginUtilities.saveSettings(this.getName(), this.settings);
+                    if (!this.settings.screenSharing) {
+                        switch (this.originalNitroStatus) {
+                            case 1:
+                                BdApi.injectCSS("screenShare", `#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(4) {
+                                    display: none;
+                                  }`);
+                                this.screenShareFix = setInterval(() => {
+                                    document.querySelector("#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(3)").click();
+                                    clearInterval(this.screenShareFix);
+                                }, 100);
+                                break;
+                            default:
+                                BdApi.injectCSS("screenShare", `#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(4) {
+                                    display: none;
+                                  }
+                                  #app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(3) {
+                                    display: none;
+                                  }
+                                  #app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(2) > div > button:nth-child(3) {
+                                    display: none;
+                                  }`);
+                                this.screenShareFix = setInterval(() => {
+                                    document.querySelector("#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(2)").click();
+                                    document.querySelector("#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(2) > div > button:nth-child(2)").click();
+                                    clearInterval(this.screenShareFix);
+                                }, 100);
+                                break;
+                        }
+                    }
+
+                    if (this.settings.screenSharing) BdApi.clearCSS("screenShare");
+                }
+
+                onStart() {
+                    this.originalNitroStatus = DiscordAPI.currentUser.discordObject.premiumType;
+                    this.saveAndUpdate();
+                    DiscordAPI.currentUser.discordObject.premiumType = 2;
+                }
+
+                onStop() {
+                    DiscordAPI.currentUser.discordObject.premiumType = this.originalNitroStatus;
+                    Patcher.unpatchAll();
+                }
+            };
+        };
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
